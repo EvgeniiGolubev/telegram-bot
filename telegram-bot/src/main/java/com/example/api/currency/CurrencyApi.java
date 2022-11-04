22dd9f4dd3c45b3b1014a9d4522a25e088c4d157
Evgenii Golubev <golubevevgenii95@yandex.ru>
2022-11-04 15:25:42 +0300

    successful deploy

--- telegram-bot/src/main/java/com/example/api/currency/CurrencyApi.java
+++ telegram-bot/src/main/java/com/example/api/currency/CurrencyApi.java
@@ -10,13 +10,13 @@ import java.io.IOException;
 
 @Component
 public class CurrencyApi {
-    public String getCurrencyValue(String value){
+    public static String getCurrencyValue(String value){
         String URL = "https://www.google.com/finance/quote/value-KZT".replaceAll("value", value);
         Document document;
         try {
             document = Jsoup.connect(URL)
                     .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36")
-                    .referrer("http://www.google.com")
+                    .referrer("https://bot-for-test-on-java.herokuapp.com/")
                     .get();
         } catch (IOException e) {
             throw new HtmlPageParsingException("Ошибка в чтении валютных котировок.");
